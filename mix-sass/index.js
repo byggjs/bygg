df72c03937f79709d3a8eb51833480460be9182b
Ole André Vadla Ravnås <ole.andre.ravnas@soundrop.com>
2014-07-10 11:44:21 +0200

    Implement a minimal sass plugin

---
+++ mix-sass/index.js
@@ -0,0 +1,82 @@
+var mix = require('mix');
+var mixIn = require('mout/object/mixIn');
+var path = require('path');
+var sass = require('node-sass');
+
+module.exports = function (options) {
+    var currentSink = null;
+
+    return function (tree) {
+        if (tree.nodes.length !== 1) {
+            throw new Error('Exactly one scss file must be specified');
+        }
+        var node = tree.nodes[0];
+
+        if (currentSink !== null) {
+            currentSink.close();
+            currentSink = null;
+        }
+
+        return new mix.Stream(function (sink) {
+            currentSink = sink;
+
+            var disposed = false;
+            var watcher = new mix.Watcher();
+            var depFiles = [];
+            watcher.on('change', function () {
+                depFiles.forEach(function (path) {
+                    watcher.remove(path);
+                });
+                depFiles = [];
+                pushCss();
+            });
+
+            pushCss();
+
+            function pushCss() {
+                var start = new Date();
+                var sassFile = path.join(node.base, node.name);
+                var stats = {};
+                sass.render(mixIn({}, options, {
+                    file: sassFile,
+                    success: function (css) {
+                        if (disposed) {
+                            return;
+                        }
+
+                        depFiles = stats.includedFiles.filter(function (path) {
+                            return path !== sassFile;
+                        });
+                        depFiles.forEach(function (path) {
+                            watcher.add(path);
+                        });
+
+                        var target = path.dirname(node.name) + '/' + path.basename(node.name, path.extname(node.name)) + '.css';
+                        var outputTree = new mix.Tree([
+                            mixIn({}, node, {
+                                name: target,
+                                data: new Buffer(css, 'utf8')
+                            })
+                        ]);
+                        sink.push(outputTree);
+
+                        console.log('generated CSS in ' + (new Date() - start) + ' ms');
+                    },
+                    error: function (error) {
+                        if (disposed) {
+                            return;
+                        }
+
+                        console.log(error);
+                    },
+                    stats: stats
+                }));
+            }
+
+            return function dispose() {
+                watcher.dispose();
+                disposed = true;
+            };
+        });
+    };
+};
