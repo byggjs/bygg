1bf8601852f84470d3beca07cd8da9a413539161
Ole André Vadla Ravnås <ole.andre.ravnas@soundrop.com>
2014-07-10 14:22:15 +0200

    Implement CSS minification

--- mix-minify-css/index.js
+++ mix-minify-css/index.js
@@ -1,6 +1,20 @@
-module.exports = function () {
+var CleanCSS = require('clean-css');
+var mix = require('mix');
+var mixIn = require('mout/object/mixIn');
+
+module.exports = function (options) {
+    options = options || {};
+
     return function (tree) {
-        // TODO: implement
-        return tree;
+        var start = new Date();
+        var nodes = tree.nodes.map(function (node) {
+            var input = node.data.toString('utf8');
+            var output = new CleanCSS(options).minify(input);
+            return mixIn({}, node, {
+                data: new Buffer(output, 'utf8')
+            });
+        });
+        console.log('minified CSS in ' + (new Date() - start) + ' ms');
+        return new mix.Tree(nodes);
     };
 };
