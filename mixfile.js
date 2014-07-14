4a11b7c5344de1b2d28c8ac9cbdce70b5aefd62d
Ole André Vadla Ravnås <ole.andre.ravnas@soundrop.com>
2014-07-14 12:32:54 +0200

    Add JSHint to the example mixfile

--- mixfile.js
+++ mixfile.js
@@ -3,6 +3,7 @@
 var autoprefixer = require('mix-autoprefixer');
 var browserify = require('mix-browserify');
 var files = require('mix-files');
+var jshint = require('mix-jshint');
 var minifyCss = require('mix-minify-css');
 var mix = require('mix');
 var rev = require('mix-rev');
@@ -13,14 +14,16 @@ var uglify = require('mix-uglify');
 var write = require('mix-write');
 
 var html = files({ base: 'src', globs: 'index.html' });
+var compileScripts = browserify({
+    extensions: ['.js', '.jsx'],
+    configure: function (b) {
+        b.transform('reactify');
+        b.require('../node_modules/react/react.js', { expose: 'react' });
+    }
+});
+compileScripts.changed.pipe(jshint());
 var scripts = files({ base: 'src', globs: 'scripts/app.jsx' })
-    .pipe(browserify({
-        extensions: ['.js', '.jsx'],
-        configure: function (b) {
-            b.transform('reactify');
-            b.require('../node_modules/react/react.js', { expose: 'react' });
-        }
-    }))
+    .pipe(compileScripts)
     .pipe(uglify());
 var styles = files({ base: 'src', globs: 'styles/app.scss' })
     .pipe(sass())
