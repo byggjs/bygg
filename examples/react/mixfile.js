29449f50941bd07da67d85abddbf490b2c2aa84e
Ole André Vadla Ravnås <ole.andre.ravnas@soundrop.com>
2014-07-14 15:57:04 +0200

    Rework the directory structure

--- examples/react/mixfile.js
+++ examples/react/mixfile.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 'use strict';
 
-var autoprefixer = require('mix-autoprefixer');
-var browserify = require('mix-browserify');
-var files = require('mix-files');
-var jshint = require('mix-jshint');
-var minifyCss = require('mix-minify-css');
+var autoprefixer = require('mix/autoprefixer');
+var browserify = require('mix/browserify');
+var files = require('mix/files');
+var jshint = require('mix/jshint');
+var minifyCss = require('mix/minify-css');
 var mix = require('mix');
-var rev = require('mix-rev');
-var sass = require('mix-sass');
-var serve = require('mix-serve');
-var stats = require('mix-stats');
-var uglify = require('mix-uglify');
-var write = require('mix-write');
+var rev = require('mix/rev');
+var sass = require('mix/sass');
+var serve = require('mix/serve');
+var stats = require('mix/stats');
+var uglify = require('mix/uglify');
+var write = require('mix/write');
 
 var html = files({ base: 'src', globs: 'index.html' });
 var compileScripts = browserify({
