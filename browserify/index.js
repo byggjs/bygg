d68440bdbdba61bd1065e06efaa59e5f6e58c9ee
Sylvain Gizard <sylvain.gizard@gmail.com>
2014-08-20 17:56:47 +0200

    Allow sass and browserify tasks to receive multiple entry points

--- browserify/index.js
+++ browserify/index.js
@@ -8,14 +8,6 @@ var path = require('path');
 var SOURCEMAPPINGURL_PREFIX = '\n//# sourceMappingURL=data:application/json;base64,';
 
 module.exports = function (options) {
-    var currentSink = null;
-    var pkgCache = {};
-    var depCache = {};
-    var upToDate = {};
-    var changedNodes = {};
-    var changedEventSink = null;
-    var firstPush = true;
-
     options = options || {};
 
     var configure = options.configure || function () {};
@@ -24,175 +16,173 @@ module.exports = function (options) {
     var bundleOptions = options.bundle || {};
     delete options.bundle;
 
-    var changed = new mix.Stream(function (sink) {
-        changedEventSink = sink;
-    });
+    var sinks = [];
+    var nodeStates = {};
 
-    function processTree(tree) {
-        if (tree.nodes.length !== 1) {
-            throw new Error('Exactly one file must be specified for browserification');
-        }
-        var node = tree.nodes[0];
-        var entrypoint = path.join(node.base, node.name);
+    return function (tree) {
+        sinks.forEach(function (sink) {
+            sink.close();
+        });
+        sinks = [];
 
-        delete depCache[entrypoint];
+        var streams = tree.nodes.map(function (node) {
+            var entrypoint = path.join(node.base, node.name);
 
-        if (currentSink !== null) {
-            currentSink.close();
-            currentSink = null;
-        }
+            var state = nodeStates[entrypoint];
+            if (state === undefined) {
+                state = nodeStates[entrypoint] = {
+                    pkgCache: {},
+                    depCache: {},
+                    upToDate: {},
+                    firstPush: true
+                };
+            }
+            var pkgCache = state.pkgCache;
+            var depCache = state.depCache;
+            var upToDate = state.upToDate;
 
-        return new mix.Stream(function (sink) {
-            currentSink = sink;
+            return new mix.Stream(function (sink) {
+                delete depCache[entrypoint];
 
-            var disposed = false;
-            var watcher = new mix.Watcher();
+                sinks.push(sink);
 
-            var b = browserify(mixIn({}, options, { basedir: node.base }));
+                var disposed = false;
+                var watcher = new mix.Watcher();
 
-            configure(b);
+                var b = browserify(mixIn({}, options, { basedir: node.base }));
 
-            b.on('package', function (file, pkg) {
-                pkgCache[file] = pkg;
-            });
-            b.on('dep', function (dep) {
-                var node = tree.findNode(function (name, base) {
-                    return dep.id.indexOf(base) === 0;
+                configure(b);
+
+                b.on('package', function (file, pkg) {
+                    pkgCache[file] = pkg;
                 });
-                if (node !== null && !upToDate[dep.id]) {
-                    upToDate[dep.id] = true;
-                    var depNode = tree.cloneNode(node);
-                    depNode.name = path.relative(node.base, dep.id);
-                    depNode.data = new Buffer(dep.source, 'utf8');
-                    changedNodes[depNode.name] = depNode;
-                }
+                b.on('dep', function (dep) {
+                    var node = tree.findNode(function (name, base) {
+                        return dep.id.indexOf(base) === 0;
+                    });
+                    if (node !== null && !upToDate[dep.id]) {
+                        upToDate[dep.id] = true;
+                        var depNode = tree.cloneNode(node);
+                        depNode.name = path.relative(node.base, dep.id);
+                        depNode.data = new Buffer(dep.source, 'utf8');
+                    }
 
-                depCache[dep.id] = dep;
-                if (dep.id !== entrypoint) {
-                    watcher.add(dep.id);
-                }
-            });
-            b.on('file', function (file) {
-                if (file !== entrypoint) {
-                    watcher.add(file);
-                }
-            });
-            b.on('bundle', function (bundle) {
-                bundle.on('transform', function (transform, mfile) {
-                    transform.on('file', function (file) {
-                        // TODO: handle file change
+                    depCache[dep.id] = dep;
+                    if (dep.id !== entrypoint) {
+                        watcher.add(dep.id);
+                    }
+                });
+                b.on('file', function (file) {
+                    if (file !== entrypoint) {
+                        watcher.add(file);
+                    }
+                });
+                b.on('bundle', function (bundle) {
+                    bundle.on('transform', function (transform, mfile) {
+                        transform.on('file', function (file) {
+                            // TODO: handle file change
+                        });
                     });
                 });
-            });
 
-            watcher.on('change', function (files) {
-                if (disposed) {
-                    return;
-                }
-
-                files.forEach(function (path) {
-                    delete depCache[path];
-                    delete upToDate[path];
-                    watcher.remove(path);
-                });
-                pushBundle();
-            });
-
-            b.add(entrypoint);
-
-            pushBundle();
-
-            function pushBundle() {
-                var buffers = [];
-                var totalLength = 0;
-                var opts = mixIn({}, bundleOptions, {
-                    includePackage: true,
-                    packageCache: pkgCache,
-                    debug: true
-                });
-                if (!firstPush) {
-                    opts.cache = depCache;
-                }
-                var start = new Date();
-                var output = b.bundle(opts);
-                output.on('data', function (buffer) {
-                    buffers.push(buffer);
-                    totalLength += buffer.length;
-                });
-                output.on('error', function (error) {
-                    console.log(error);
-                });
-                output.on('end', function () {
+                watcher.on('change', function (files) {
                     if (disposed) {
                         return;
                     }
 
-                    console.log('generated JS in ' + (new Date() - start) + ' ms');
-
-                    firstPush = false;
-
-                    var source = Buffer.concat(buffers).toString('utf8');
-                    var sourceMapData = null;
-                    var sourceMapUrlStart = source.lastIndexOf(SOURCEMAPPINGURL_PREFIX);
-                    if (sourceMapUrlStart !== -1) {
-                        var sourceMapUrlEnd = source.indexOf('\n', sourceMapUrlStart + SOURCEMAPPINGURL_PREFIX.length);
-                        if (sourceMapUrlEnd === -1) {
-                            sourceMapUrlEnd = source.length;
-                        }
-                        var originalSourceMap = JSON.parse(new Buffer(source.substring(sourceMapUrlStart + SOURCEMAPPINGURL_PREFIX.length, sourceMapUrlEnd), 'base64').toString('utf8'));
-                        var modifiedSourceMap = fixupSourceMap(originalSourceMap);
-                        sourceMapData = new Buffer(JSON.stringify(modifiedSourceMap), 'utf8');
-                        source = source.substring(0, sourceMapUrlStart) + source.substring(sourceMapUrlEnd);
-                    }
-
-                    var outputNode = tree.cloneNode(node);
-                    var outputPrefix = path.dirname(node.name) + '/';
-                    if (outputPrefix === './') {
-                        outputPrefix = '';
-                    }
-                    outputNode.name = outputPrefix + path.basename(node.name, path.extname(node.name)) + '.js';
-                    outputNode.metadata.mime = 'application/javascript';
-                    if (sourceMapData !== null) {
-                        source += '\n//# sourceMappingURL=./' + path.basename(outputNode.name) + '.map';
-                        outputNode.metadata.sourceMap = outputNode.siblings.length;
-                        outputNode.siblings.push({
-                            name: outputNode.name + '.map',
-                            data: sourceMapData,
-                            stat: node.stat
-                        });
-                    }
-                    outputNode.data = new Buffer(source, 'utf8');
-
-                    var outputTree = new mix.Tree([outputNode]);
-                    sink.push(outputTree);
-
-                    var nodes = [];
-                    Object.keys(changedNodes).forEach(function (name) {
-                        nodes.push(changedNodes[name]);
+                    files.forEach(function (path) {
+                        delete depCache[path];
+                        delete upToDate[path];
+                        watcher.remove(path);
                     });
-                    changedNodes = {};
-                    if (nodes.length > 0) {
-                        changedEventSink.push(new mix.Tree(nodes));
+                    pushBundle();
+                });
+
+                b.add(entrypoint);
+
+                pushBundle();
+
+                function pushBundle() {
+                    var buffers = [];
+                    var totalLength = 0;
+                    var opts = mixIn({}, bundleOptions, {
+                        includePackage: true,
+                        packageCache: pkgCache,
+                        debug: true
+                    });
+                    if (!state.firstPush) {
+                        opts.cache = depCache;
                     }
-                });
-            }
+                    var start = new Date();
+                    var output = b.bundle(opts);
+                    output.on('data', function (buffer) {
+                        buffers.push(buffer);
+                        totalLength += buffer.length;
+                    });
+                    output.on('error', function (error) {
+                        console.log(error);
+                    });
+                    output.on('end', function () {
+                        if (disposed) {
+                            return;
+                        }
 
-            function fixupSourceMap(sourceMap) {
-                var result = mixIn({}, sourceMap);
-                result.sources = sourceMap.sources.map(function (source) {
-                    return path.relative(node.base, source);
-                });
-                return result;
-            }
+                        console.log('generated JS in ' + (new Date() - start) + ' ms');
 
-            return function dispose() {
-                watcher.dispose();
-                disposed = true;
-            };
+                        state.firstPush = false;
+
+                        var source = Buffer.concat(buffers).toString('utf8');
+                        var sourceMapData = null;
+                        var sourceMapUrlStart = source.lastIndexOf(SOURCEMAPPINGURL_PREFIX);
+                        if (sourceMapUrlStart !== -1) {
+                            var sourceMapUrlEnd = source.indexOf('\n', sourceMapUrlStart + SOURCEMAPPINGURL_PREFIX.length);
+                            if (sourceMapUrlEnd === -1) {
+                                sourceMapUrlEnd = source.length;
+                            }
+                            var originalSourceMap = JSON.parse(new Buffer(source.substring(sourceMapUrlStart + SOURCEMAPPINGURL_PREFIX.length, sourceMapUrlEnd), 'base64').toString('utf8'));
+                            var modifiedSourceMap = fixupSourceMap(originalSourceMap);
+                            sourceMapData = new Buffer(JSON.stringify(modifiedSourceMap), 'utf8');
+                            source = source.substring(0, sourceMapUrlStart) + source.substring(sourceMapUrlEnd);
+                        }
+
+                        var outputNode = tree.cloneNode(node);
+                        var outputPrefix = path.dirname(node.name) + '/';
+                        if (outputPrefix === './') {
+                            outputPrefix = '';
+                        }
+                        outputNode.name = outputPrefix + path.basename(node.name, path.extname(node.name)) + '.js';
+                        outputNode.metadata.mime = 'application/javascript';
+                        if (sourceMapData !== null) {
+                            source += '\n//# sourceMappingURL=./' + path.basename(outputNode.name) + '.map';
+                            outputNode.metadata.sourceMap = outputNode.siblings.length;
+                            outputNode.siblings.push({
+                                name: outputNode.name + '.map',
+                                data: sourceMapData,
+                                stat: node.stat
+                            });
+                        }
+                        outputNode.data = new Buffer(source, 'utf8');
+
+                        var outputTree = new mix.Tree([outputNode]);
+                        sink.push(outputTree);
+                    });
+                }
+
+                function fixupSourceMap(sourceMap) {
+                    var result = mixIn({}, sourceMap);
+                    result.sources = sourceMap.sources.map(function (source) {
+                        return path.relative(node.base, source);
+                    });
+                    return result;
+                }
+
+                return function dispose() {
+                    watcher.dispose();
+                    disposed = true;
+                };
+            });
         });
+
+        return mix.combine.apply(null, streams);
     };
-
-    Object.defineProperty(processTree, 'changed', { value: changed });
-
-    return processTree;
 };
--- sass/index.js
+++ sass/index.js
@@ -6,80 +6,79 @@ var path = require('path');
 var sass = require('node-sass');
 
 module.exports = function (options) {
-    var currentSink = null;
+    var sinks = [];
 
     return function (tree) {
-        if (tree.nodes.length !== 1) {
-            throw new Error('Exactly one scss file must be specified');
-        }
-        var node = tree.nodes[0];
-
-        if (currentSink !== null) {
-            currentSink.close();
-            currentSink = null;
-        }
-
-        return new mix.Stream(function (sink) {
-            currentSink = sink;
-
-            var disposed = false;
-            var watcher = new mix.Watcher();
-            var depFiles = [];
-            watcher.on('change', function () {
-                depFiles.forEach(function (path) {
-                    watcher.remove(path);
-                });
-                depFiles = [];
-                pushCss();
-            });
-
-            pushCss();
-
-            function pushCss() {
-                var sassFile = path.join(node.base, node.name);
-                var stats = {};
-                var start = new Date();
-                sass.render(mixIn({}, options, {
-                    file: sassFile,
-                    success: function (css) {
-                        if (disposed) {
-                            return;
-                        }
-
-                        console.log('generated CSS in ' + (new Date() - start) + ' ms');
-
-                        depFiles = stats.includedFiles.filter(function (path) {
-                            return path !== sassFile;
-                        });
-                        depFiles.forEach(function (path) {
-                            watcher.add(path);
-                        });
-
-                        var outputNode = tree.cloneNode(node);
-                        var outputPrefix = path.dirname(node.name) + '/';
-                        if (outputPrefix === './') {
-                            outputPrefix = '';
-                        }
-                        outputNode.name = outputPrefix + path.basename(node.name, path.extname(node.name)) + '.css';
-                        outputNode.data = new Buffer(css, 'utf8');
-                        outputNode.metadata.mime = 'text/css';
-                        sink.push(new mix.Tree([outputNode]));
-                    },
-                    error: function (error) {
-                        if (disposed) {
-                            return;
-                        }
-
-                        console.log(error);
-                    },
-                    stats: stats
-                }));
-            }
-
-            return function dispose() {
-                watcher.dispose();
-                disposed = true;
-            };
+        sinks.forEach(function (sink) {
+            sink.close();
         });
+        sinks = [];
+
+        var streams = tree.nodes.map(function (node) {
+            return new mix.Stream(function (sink) {
+                sinks.push(sink);
+
+                var disposed = false;
+                var watcher = new mix.Watcher();
+                var depFiles = [];
+                watcher.on('change', function () {
+                    depFiles.forEach(function (path) {
+                        watcher.remove(path);
+                    });
+                    depFiles = [];
+                    pushCss();
+                });
+
+                pushCss();
+
+                function pushCss() {
+                    var sassFile = path.join(node.base, node.name);
+                    var stats = {};
+                    var start = new Date();
+                    sass.render(mixIn({}, options, {
+                        file: sassFile,
+                        success: function (css) {
+                            if (disposed) {
+                                return;
+                            }
+
+                            console.log('generated CSS in ' + (new Date() - start) + ' ms');
+
+                            depFiles = stats.includedFiles.filter(function (path) {
+                                return path !== sassFile;
+                            });
+                            depFiles.forEach(function (path) {
+                                watcher.add(path);
+                            });
+
+                            var outputNode = tree.cloneNode(node);
+                            var outputPrefix = path.dirname(node.name) + '/';
+                            if (outputPrefix === './') {
+                                outputPrefix = '';
+                            }
+                            outputNode.name = outputPrefix + path.basename(node.name, path.extname(node.name)) + '.css';
+                            outputNode.data = new Buffer(css, 'utf8');
+                            outputNode.metadata.mime = 'text/css';
+                            sink.push(new mix.Tree([outputNode]));
+                        },
+                        error: function (error) {
+                            if (disposed) {
+                                return;
+                            }
+
+                            console.log(error);
+                        },
+                        stats: stats
+                    }));
+                }
+
+                return function dispose() {
+                    watcher.dispose();
+                    disposed = true;
+                };
+            });
+        });
+
+        return mix.combine.apply(null, streams);
     };
 };
