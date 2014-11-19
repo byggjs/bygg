5dea87d8821c9e81e6058121524098bf912ae914
Sylvain Gizard <sylvain.gizard@gmail.com>
2014-11-19 17:01:56 +0100

    Proper error handling for autoprefixer

--- autoprefixer/index.js
+++ autoprefixer/index.js
@@ -10,8 +10,8 @@ module.exports = function () {
     var constraints = arguments.length > 0 ? Array.prototype.slice.call(arguments) : DEFAULT_CONSTRAINTS;
 
     return function (tree) {
-        var start = new Date();
         var nodes = tree.nodes.map(function (node) {
+            var start = new Date();
             var input = node.data.toString('utf8');
 
             var prevSourceMap = node.siblings[node.metadata.sourceMap];
@@ -24,15 +24,24 @@ module.exports = function () {
                 }
             };
 
-            var result = autoprefixer.call(constraints).process(input, opts);
+            try {
+                var outputNode = tree.cloneNode(node);
+                var result = autoprefixer({ browsers: constraints }).process(input, opts);
+                outputNode.data = new Buffer(result.css, 'utf8');
 
-            var outputNode = tree.cloneNode(node);
-            outputNode.data = new Buffer(result.css, 'utf8');
+                var sourceMap = JSON.parse(result.map);
+                mix.tree.sourceMap.set(outputNode, sourceMap);
 
-            var sourceMap = JSON.parse(result.map);
-            mix.tree.sourceMap.set(outputNode, sourceMap);
+                mix.logger.log('autoprefixer', 'Prefixed ' + node.name, new Date() - start);
+                return outputNode;
+            } catch (e) {
+                mix.logger.error('autoprefixer', e.message);
+            }
 
-            return outputNode;
+            return undefined;
+        })
+        .filter(function (node) {
+            return node !== undefined;
         });
 
         return mix.tree(nodes);
