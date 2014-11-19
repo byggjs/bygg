3c1599a090dc0555858a8ca9c07b4a02d672a3bb
Sylvain Gizard <sylvain.gizard@gmail.com>
2014-11-19 14:10:20 +0100

    add svg-sprite and svgo plugins

---
+++ svgo/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var mix = require('mix');
+var SVGO = require('svgo');
+
+module.exports = function (options) {
+    options = options || {};
+
+    return function (tree) {
+        var svgo = new SVGO(options);
+        var signal = mix.signal();
+        var nodes = [];
+        var processed = 0;
+
+        tree.nodes.forEach(function (node, index) {
+            var input = node.data.toString('utf8');
+
+            svgo.optimize(input, function(result) {
+                if (result.error) {
+                    processed++;
+                    mix.logger.error('svgo', result.error);
+                    return;
+                }
+
+                var outputNode = tree.cloneNode(node);
+                outputNode.data = new Buffer(result.data, 'utf8');
+                nodes.push(outputNode);
+                processed++;
+
+                if (processed === tree.nodes.length) {
+                    signal.push(mix.tree(nodes));
+                }
+            });
+        });
+
+        return signal;
+    };
+};
